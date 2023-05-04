f5cf172274c510ac01f232f394c3647e65b9366f
Arturas <artukras@gmail.com>
2023-05-04 12:07:44 +0300

    cart html updates

--- resources/views/client/cart.blade.php
+++ resources/views/client/cart.blade.php
@@ -9,24 +9,24 @@
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="frontend/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/css/animate.css">
     
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="frontend/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="frontend/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="frontend/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="frontend/css/aos.css">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="frontend/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="frontend/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="frontend/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="frontend/css/flaticon.css">
+    <link rel="stylesheet" href="frontend/css/icomoon.css">
+    <link rel="stylesheet" href="frontend/css/style.css">
   </head>
   <body class="goto-here">
 		<div class="py-1 bg-primary">
@@ -72,7 +72,7 @@
 
 	<!-- start content -->
 
-    <div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+    <div class="hero-wrap hero-bread" style="background-image: url('frontend/images/bg_1.jpg');">
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
@@ -103,7 +103,7 @@
 						      <tr class="text-center">
 						        <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
 						        
-						        <td class="image-prod"><div class="img" style="background-image:url(images/product-3.jpg);"></div></td>
+						        <td class="image-prod"><div class="img" style="background-image:url(frontend/images/product-3.jpg);"></div></td>
 						        
 						        <td class="product-name">
 						        	<h3>Bell Pepper</h3>
@@ -127,7 +127,7 @@
 						      <tr class="text-center">
 						        <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
 						        
-						        <td class="image-prod"><div class="img" style="background-image:url(images/product-4.jpg);"></div></td>
+						        <td class="image-prod"><div class="img" style="background-image:url(frontend/images/product-4.jpg);"></div></td>
 						        
 						        <td class="product-name">
 						        	<h3>Bell Pepper</h3>
@@ -313,22 +313,22 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
   
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <script src="frontend/js/jquery.min.js"></script>
+  <script src="frontend/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="frontend/js/popper.min.js"></script>
+  <script src="frontend/js/bootstrap.min.js"></script>
+  <script src="frontend/js/jquery.easing.1.3.js"></script>
+  <script src="frontend/js/jquery.waypoints.min.js"></script>
+  <script src="frontend/js/jquery.stellar.min.js"></script>
+  <script src="frontend/js/owl.carousel.min.js"></script>
+  <script src="frontend/js/jquery.magnific-popup.min.js"></script>
+  <script src="frontend/js/aos.js"></script>
+  <script src="frontend/js/jquery.animateNumber.min.js"></script>
+  <script src="frontend/js/bootstrap-datepicker.js"></script>
+  <script src="frontend/js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
+  <script src="frontend/js/google-map.js"></script>
+  <script src="frontend/js/main.js"></script>
 
   <script>
 		$(document).ready(function(){
