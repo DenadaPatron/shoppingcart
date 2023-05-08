1c3355440f42986cb31ba3acf5f4a0952519f927
Arturas <artukras@gmail.com>
2023-05-09 00:53:30 +0300

    create accoutn functionality & model

--- resources/views/client/signup.blade.php
+++ resources/views/client/signup.blade.php
@@ -32,7 +32,8 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('frontend/login/images/bg-01.jpg');">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" action="{{url('/create_account')}}" method="POST">
+					@csrf
 						<a href="{{url('/')}}">
 							<span class="login100-form-logo">
 									<i class="zmdi zmdi-landscape"></i>
@@ -44,12 +45,12 @@
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+						<input class="input100" type="email" name="email" placeholder="Username">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="password" placeholder="Password">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
 
@@ -61,9 +62,10 @@
 					</div> --}}
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						{{-- <button class="login100-form-btn">
 							Sign up
-						</button>
+						</button> --}}
+						<input type="submit" class="login100-form-btn">
 					</div>
 
 					<div class="text-center p-t-90">
